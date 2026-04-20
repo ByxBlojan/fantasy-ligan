c17fd32ec2dcbe7e26ad605146b622e1d3884631
Lindqvist Magnus <magnus.lindqvist@softronic.se>
2026-04-20 09:26:21 +0200

    fix: lägg till base-url för GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/fantasy-ligan/',
   plugins: [react(), tailwindcss()],
 })
